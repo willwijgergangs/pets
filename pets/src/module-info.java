6320cf22f1da64a2266e3e5922ad2914302671c7
Will.Wijgergangs <Will.Wijgergangs@LTSIA-HD86WF2>
2021-10-28 19:26:42 +0200

    Initial pets release

---
+++ pets/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Will.Wijgergangs
+ *
+ */
+module pets {
+}
---
+++ pets/src/pets/BigAnimals.java
@@ -0,0 +1,10 @@
+package pets;
+
+public class BigAnimals {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
